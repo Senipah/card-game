aa0263c60a1c55b90903b3e865bf888895cb92f0
Senipah <n.morgan86@googlemail.com>
2020-03-22 14:56:30 +0000

    Updates

---
+++ precache-manifest.df8823418ddf1eaaa7b83d9a3a81ddc7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e88af656d180e13613be21d8579c4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d05c9381e91051f0d39",
+    "url": "/static/js/2.c82d657a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c82d657a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d628e0b4d368363cb956",
+    "url": "/static/js/main.887430d6.chunk.js"
+  },
+  {
+    "revision": "39887028c419cb0f762d",
+    "url": "/static/js/runtime-main.567273c5.js"
+  },
+  {
+    "revision": "0b3250e02cd91132999156247584c912",
+    "url": "/static/media/Drums.0b3250e0.png"
+  },
+  {
+    "revision": "6c64c7186b0600d091c83b6fe55240de",
+    "url": "/static/media/Guitar.6c64c718.png"
+  },
+  {
+    "revision": "e7816627361292bbc33131778949c371",
+    "url": "/static/media/Harp.e7816627.png"
+  },
+  {
+    "revision": "7d5ae6c3b99ea64be2f6ae7750dbadbb",
+    "url": "/static/media/Maracas.7d5ae6c3.png"
+  },
+  {
+    "revision": "f29472b2b7f18bd1fc867c89a8c2f697",
+    "url": "/static/media/Piano.f29472b2.png"
+  },
+  {
+    "revision": "eaa6a01bd0258345986e814114d7888f",
+    "url": "/static/media/Saxophone.eaa6a01b.png"
+  },
+  {
+    "revision": "879566007a66dcbd3f722bff2dcb848f",
+    "url": "/static/media/Violin.87956600.png"
+  },
+  {
+    "revision": "dc75f22fdbb56380cac4a966a847c394",
+    "url": "/static/media/Xylophone.dc75f22f.png"
+  },
+  {
+    "revision": "bbe05aad35f0c1f37101d2cbc7c9a033",
+    "url": "/static/media/background.bbe05aad.png"
+  },
+  {
+    "revision": "8ca9063d999b21e9169faea4f3c61f60",
+    "url": "/static/media/logo.8ca9063d.png"
+  }
+]);
